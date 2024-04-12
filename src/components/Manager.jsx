55850f8b0e57787b32799052ebf0ec486a215720
Shubham Kumar Vishwakarma <vis.shubham2002@gmail.com>
2024-04-12 14:38:53 +0530

    Updated All Components and Replaced it with Daisy UI Components

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -3,32 +3,65 @@ import React from 'react'
 const Manager = () => {
     return (
         <>
-            <div className="absolute top-0 z-[-2] h-screen w-screen rotate-180 transform bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]"></div>
-            <div className="container p-8 mx-auto">
-                <form action="">
+            {/* Form */}
+            <div className="container px-8 py-2">
+                <form action="" className='w-[600px] mx-auto'>
                     <div className="flex flex-col text-center w-full">
                         <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Password Manager OP</h1>
                     </div>
-                    {/* Site URL */}
-                    <div className="flex lg:w-2/3 w-full sm:flex-row flex-col mx-auto px-8 sm:space-x-4 sm:space-y-0 space-y-4 sm:px-0 items-end">
-                        <div className="relative flex-grow w-full">
-                            <label for="full-name" className="leading-7 text-sm text-gray-600">Site URL</label>
-                            <input type="text" id="full-name" name="full-name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-                        </div>
-                    </div>
-                    {/* USername and Password */}
-                    <div className="flex lg:w-2/3 w-full sm:flex-row flex-col mx-auto px-8 sm:space-x-4 sm:space-y-0 space-y-4 sm:px-0 items-end">
-                        <div className="relative flex-grow w-full">
-                            <label for="full-name" className="leading-7 text-sm text-gray-600">Username</label>
-                            <input type="text" id="full-name" name="full-name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-                        </div>
-                        <div className="relative flex-grow w-full">
-                            <label for="password" className="leading-7 text-sm text-gray-600">Password</label>
-                            <input type="password" id="password" name="password" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-                        </div>
-                        <button className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Save</button>
+                    <label className="input input-bordered flex items-center mb-5">
+                        Site URL
+                        <input type="text" className="grow" placeholder="Daisy" />
+                    </label>
+                    <label className="input input-bordered flex items-center mb-5">
+                        Username
+                        <input type="text" className="grow" placeholder="daisy@site.com" />
+                    </label>
+                    <label className="input input-bordered flex items-center mb-5">
+                        Password
+                        <input type="text" className="grow" placeholder="daisy@site.com" />
+                    </label>
+                    <div>
+                        <button className="btn bg-green-500 text-white w-full">Success</button>
                     </div>
                 </form>
+            </div>
+            {/* Table */}
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        <tr className="bg-base-200">
+                            <th>1</th>
+                            <td>Cy Ganderton</td>
+                            <td>Quality Control Specialist</td>
+                            <td>Blue</td>
+                        </tr>
+                        {/* row 2 */}
+                        <tr>
+                            <th>2</th>
+                            <td>Hart Hagerty</td>
+                            <td>Desktop Support Technician</td>
+                            <td>Purple</td>
+                        </tr>
+                        {/* row 3 */}
+                        <tr>
+                            <th>3</th>
+                            <td>Brice Swyre</td>
+                            <td>Tax Accountant</td>
+                            <td>Red</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </>
     )
