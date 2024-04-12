c4221fb82c83c4d9ac87ad09dec26814e0245334
Shubham Kumar Vishwakarma <vis.shubham2002@gmail.com>
2024-04-12 15:01:24 +0530

    UI Almost Done

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -5,63 +5,63 @@ const Manager = () => {
         <>
             {/* Form */}
             <div className="container px-8 py-2">
-                <form action="" className='w-[600px] mx-auto'>
+                <form action="" className='max-w-[600px] mx-auto'>
                     <div className="flex flex-col text-center w-full">
                         <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Password Manager OP</h1>
                     </div>
                     <label className="input input-bordered flex items-center mb-5">
-                        Site URL
-                        <input type="text" className="grow" placeholder="Daisy" />
+                        Site URL &nbsp;
+                        <input type="text" className="grow" />
                     </label>
                     <label className="input input-bordered flex items-center mb-5">
-                        Username
-                        <input type="text" className="grow" placeholder="daisy@site.com" />
+                        Username &nbsp;
+                        <input type="text" className="grow" />
                     </label>
                     <label className="input input-bordered flex items-center mb-5">
-                        Password
-                        <input type="text" className="grow" placeholder="daisy@site.com" />
+                        Password &nbsp;
+                        <input type="text" className="grow" />
                     </label>
                     <div>
-                        <button className="btn bg-green-500 text-white w-full">Success</button>
+                        <button className="btn btn-active btn-neutral w-full">Success</button>
                     </div>
                 </form>
             </div>
             {/* Table */}
-            <div className="overflow-x-auto">
-                <table className="table">
-                    {/* head */}
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th>Name</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {/* row 1 */}
-                        <tr className="bg-base-200">
-                            <th>1</th>
-                            <td>Cy Ganderton</td>
-                            <td>Quality Control Specialist</td>
-                            <td>Blue</td>
-                        </tr>
-                        {/* row 2 */}
-                        <tr>
-                            <th>2</th>
-                            <td>Hart Hagerty</td>
-                            <td>Desktop Support Technician</td>
-                            <td>Purple</td>
-                        </tr>
-                        {/* row 3 */}
-                        <tr>
-                            <th>3</th>
-                            <td>Brice Swyre</td>
-                            <td>Tax Accountant</td>
-                            <td>Red</td>
-                        </tr>
-                    </tbody>
-                </table>
+            <div className='container mb-5 min-h-72'>
+                <h2 class="sm:text-2xl text-xl title-font font-medium text-gray-900 mt-4 mb-4 text-center">Your Passwords</h2>
+                <div className="overflow-x-auto max-w-[1000px] mx-auto">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Site</th>
+                                <th>Username</th>
+                                <th>Password</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr className="bg-base-200">
+                                <th>1</th>
+                                <td>Cy Ganderton</td>
+                                <td>Quality Control Specialist</td>
+                                <td>Blue</td>
+                                <td>
+                                    <button className='me-3 text-purple-600'>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                        </svg>
+                                    </button>
+                                    <button className='text-red-600'>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                        </svg>
+                                    </button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </>
     )
