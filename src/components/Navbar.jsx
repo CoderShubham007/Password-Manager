9ab9867416f784cf6ee7a7671884b1353ed7434d
Shubham Kumar Vishwakarma <vis.shubham2002@gmail.com>
2024-04-14 14:20:19 +0530

    Added Link in Header And Footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className="navbar bg-neutral text-neutral-content">
-      <button className="btn btn-ghost text-xl">daisyUI</button>
+      <a href='/' className="btn btn-ghost text-xl">Password Manager</a>
     </div>
   )
 }
