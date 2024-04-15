3d7269fa06584e9dc690f25ca4b8e5c5d5141ec6
Shubham Kumar Vishwakarma <vis.shubham2002@gmail.com>
2024-04-15 18:09:15 +0530

    Added BAckground Color to Web Page

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -72,6 +72,8 @@ const Manager = () => {
             />
             {/* Same as */}
             <ToastContainer />
+            {/* Background Color */}
+            <div class="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
             {/* Form */}
             <div className="container px-8 py-2">
                 <form className='max-w-[600px] mx-auto' onSubmit={savePassword}>
